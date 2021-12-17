3bde1dbf43dd6638b7e139321f497ebff785d977
Gabriel Ruizz <garciagabriel1945@gmail.com>
2021-12-17 15:30:52 -0800

    pass event parameter to createTaskhander func to add event.preventDefault(), submit instead of click for addEventlistener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
-const buttonEl = document.querySelector("#save-task");
+const formEl = document.querySelector('#task-form');
 const tasksToDoEl = document.querySelector('#tasks-to-do');
 
-const createTaskHandler = () => {
+const createTaskHandler = event => {
+
+    event.preventDefault();
+
     const listItemEl = document.createElement('li');
     listItemEl.className = 'task-item';
     listItemEl.textContent = 'New Task';
     tasksToDoEl.appendChild(listItemEl);
 }
 
-buttonEl.addEventListener('click', createTaskHandler);
+formEl.addEventListener('submit', createTaskHandler);
