ce83e930adbdcbdd2aafff730820e22bfbb06f37
Gabriel Ruizz <garciagabriel1945@gmail.com>
2022-01-03 21:27:14 -0800

    pushing work done in 4.2.5

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,22 @@ const createTaskHandler = event => {
 
     event.preventDefault();
 
+    const taskNameInput = document.querySelector("input[name='task-name']").value;
+    const taskTypeInput = document.querySelector("select[name='task-type']").value;
+
     const listItemEl = document.createElement('li');
     listItemEl.className = 'task-item';
-    listItemEl.textContent = 'New Task';
+
+    // div to hold task info and to list item
+    const taskInfoEl = document.createElement('div');
+    // give it a class name
+    taskInfoEl.className = 'task-info';
+    // add HTML cntent to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 }
 
