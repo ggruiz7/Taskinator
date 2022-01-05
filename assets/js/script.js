ca132699b6f56cf0afd718e05e32ae42a885684b
Gabriel Ruizz <garciagabriel1945@gmail.com>
2022-01-04 21:14:11 -0800

    updating arr of obj containing task info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
 let taskIdCounter = 0;
+
 const formEl = document.querySelector('#task-form');
 const tasksToDoEl = document.querySelector('#tasks-to-do');
 const tasksInProgressEl = document.querySelector('#tasks-in-progress');
 const tasksCompletedEl = document.querySelector('#tasks-completed');
 const pageContentEl = document.querySelector('#page-content');
 
-const taskFormHandler = event => {
+const tasks = []
 
+const taskFormHandler = event => {
     event.preventDefault();
 
     const taskNameInput = document.querySelector("input[name='task-name']").value;
@@ -20,9 +22,9 @@ const taskFormHandler = event => {
 
     formEl.reset();
 
+    // check if task is new or one being edited by seeing if it has a data-task-id attribute
     const isEdit = formEl.hasAttribute('data-task-id');
 
-    // has a data attribute, so get task id and call function to complete edit process
     if (isEdit) {
         const taskId = formEl.getAttribute('data-task-id');
         completeEditTask(taskNameInput, taskTypeInput, taskId);
@@ -31,7 +33,8 @@ const taskFormHandler = event => {
     else {
         const taskDataObj = {
             name: taskNameInput,
-            type: taskTypeInput
+            type: taskTypeInput,
+            status: 'to do'
         };
         createTaskEl(taskDataObj);
     }
@@ -39,23 +42,26 @@ const taskFormHandler = event => {
 }
 
 const createTaskEl = taskDataObj => {
+    console.log(taskDataObj);
+    console.log(taskDataObj.status);
+
     const listItemEl = document.createElement('li');
     listItemEl.className = 'task-item';
-
-    // add task id as custom attribute
     listItemEl.setAttribute('data-task-id', taskIdCounter);
     
     const taskInfoEl = document.createElement('div');
     taskInfoEl.className = 'task-info';
     taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
-    
     listItemEl.appendChild(taskInfoEl);
 
-    // store createTaskActions in var, then append it to the list item
+    // task actions for task: edit, delete
     const taskActionsEl = createTaskActions(taskIdCounter);
     listItemEl.appendChild(taskActionsEl);
-    
     tasksToDoEl.appendChild(listItemEl);
+
+    taskDataObj.id = taskIdCounter;
+    
+    tasks.push(taskDataObj);
 
     // increase task counter for next unique id
     taskIdCounter++;
@@ -70,7 +76,6 @@ const createTaskActions = taskId => {
     editButtonEl.textContent = 'Edit';
     editButtonEl.className = 'btn edit-btn';
     editButtonEl.setAttribute('data-task-id', taskId);
-
     actionContainerEl.appendChild(editButtonEl);
 
     // create delete button
@@ -78,15 +83,13 @@ const createTaskActions = taskId => {
     deleteButtonEl.textContent = 'Delete';
     deleteButtonEl.className = 'btn delete-btn';
     deleteButtonEl.setAttribute('data-task-id', taskId);
-
     actionContainerEl.appendChild(deleteButtonEl);
 
-    // create task item dropdown
+    // create status dropdown
     const statusSelectEl = document.createElement('select');
     statusSelectEl.className = 'select-status';
     statusSelectEl.setAttribute('name', 'status-change');
     statusSelectEl.setAttribute('data-task-id', taskId);
-
     actionContainerEl.appendChild(statusSelectEl);
 
     const statusChoices = ['To Do', 'In Progress', 'Completed'];
@@ -105,7 +108,6 @@ const createTaskActions = taskId => {
     return actionContainerEl;
 }
 
-
 const taskButtonHandler = event => {
     // get target element from event
     const targetEl = event.target;
@@ -115,9 +117,8 @@ const taskButtonHandler = event => {
         const taskId = targetEl.getAttribute('data-task-id');
         editTask(taskId);
     }
-    
     // delete button clicked
-    if (targetEl.matches('.delete-btn')) {
+    else if (targetEl.matches('.delete-btn')) {
         const taskId = targetEl.getAttribute('data-task-id');
         deleteTask(taskId);
     }
@@ -148,27 +149,52 @@ const completeEditTask = (taskName, taskType, taskId) => {
     // set new values
     taskSelected.querySelector('h3.task-name').textContent = taskName;
     taskSelected.querySelector('span.task-type').textContent = taskType;
+
+    // loop throught tasks array and task object with new content
+    for (let i=0; i<tasks.length; i++) {
+        if (tasks[i].id === parseInt(taskId)) {
+            tasks[i].name = taskName;
+            tasks[i].type = taskType;
+        }
+    }
+    console.log(tasks);
     
     alert('Task Updated!');
     
+    // remove data attribute from form
     formEl.removeAttribute('data-task-id');
+    // update fromEl button to go back to saying 'add task'
     document.querySelector('#save-task').textContent = 'Add Task';
 }
 
 const deleteTask = taskId => {
     const taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
     taskSelected.remove();
+
+    // create new array to hold updated list of tasks
+    const updatedTaskArr =[]
+
+    // loop through current tasks
+    for (let i=0; i<tasks.length; i++) {
+        // if tasks[i].id !== taskId, keep that task and push it into the new array
+        if (tasks[i].id !== parseInt(taskId)) {
+            updatedTaskArr.push(tasks[i]);
+        }
+    }
+
+    // reassign tasks array to be the same as the updated tasks array
+    tasks = updatedTaskArr;
 }
 
 const taskStatusChangeHandler = event => {
     // get the task item's id
     const taskId = event.target.getAttribute('data-task-id');
 
-    // get the currently seleted option's value and convert it to lower case
-    const statusValue = event.target.value.toLowerCase();
-
     // find the parent task item element based on the id
     const taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    // get the currently seleted option's value and convert it to lower case
+    const statusValue = event.target.value.toLowerCase();
 
     if (statusValue === 'to do') {
         tasksToDoEl.appendChild(taskSelected);
@@ -179,8 +205,19 @@ const taskStatusChangeHandler = event => {
     else if (statusValue === 'completed') {
         tasksCompletedEl.appendChild(taskSelected);
     }
+
+    // update tasks in tasks array
+    for (let i=0; i<tasks.length; i++) {
+        if(tasks[i].id === parseInt(taskId)) {
+            tasks[i].status = statusValue;
+        }
+    }
+    console.log(tasks);
 }
 
+// add new task
 formEl.addEventListener('submit', taskFormHandler);
+// edit and delete
 pageContentEl.addEventListener('click', taskButtonHandler);
+// change task status
 pageContentEl.addEventListener('change', taskStatusChangeHandler);
